1eb4711a2bec49f5acbc4495b574597b107296c5
Ollie <ollie.pye@live.com>
2023-07-07 12:06:02 +1000

    Finished tutorial video

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 def home(request):
@@ -54,6 +54,43 @@ def customer_record(request, pk):
         #look up record
         customer_record = Record.objects.get(id=pk)
         return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You must sign in to view that page")
+        return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted")
+        return redirect('home')
+    else:
+        messages.success(request, "You must sign in to action that request")
+        return redirect('home')
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record added")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "You must sign in to view that page")
+        return redirect('home')
+
+def edit_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record Updated")
+            return redirect('home')
+        return render(request, 'edit_record.html', {'form':form})
     else:
         messages.success(request, "You must sign in to view that page")
         return redirect('home')
